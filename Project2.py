c22714cd1cd11919ef982ae1d1bb6feb1c82c464
orco2oilee <132207454+orco2oilee@users.noreply.github.com>
2025-09-21 17:18:46 +0000

    Project2.py updated.

---
+++ Project2.py
@@ -0,0 +1,20 @@
+#Hello guys! Whoever finish the project, please delete this comments.
+#If Joshua finishes first, please leave a space for Nelbat to add his part.
+#If Nelbat finishes first, please leave a space for Joshua to add his part.
+#If you have any questionsor when you finished the project, please send me a mail.
+
+player1_wins = 0
+player2_wins = 0
+draws = 0
+round_number = 1
+
+choices = ["rock", "paper", "scissors"]
+
+while True:
+    print(f" Round {round_number} ")
+    
+    #Nelbat's part here.
+    
+    #Joshua's part here.
+    
+    round_number += 1
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Project-2-RPS
+# Project-2-RPS 
+Hello Joshua! I hope you’re feeling better and taking care of yourself while you’re sick. I also hope you’re working on your part yourself and not just using AI. 
+**I hope you’re feeling better than before!**
